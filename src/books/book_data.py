49e2b639ca977f73835e0f8dc0ba0af1e3db959b
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-06-29 10:28:36 +0300

    First commit, creating a CRUD endpoints in book logic and schemas

---
+++ src/books/book_data.py
@@ -0,0 +1,56 @@
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O`Reilly Media",
+        "publisher_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English"
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "AAntonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "publisher_date": "2021-01-19",
+        "page_count": 1023,
+        "language": "English"
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "publisher_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English"
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "O`Riley Media",
+        "publisher_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English"
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Spring Inc",
+        "publisher_date": "2021-01-01",
+        "page_count": 9288,
+        "language": "English"
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O`Reilly Media",
+        "publisher_date": "2021-21-01",
+        "page_count": 3006,
+        "language": "English"
+    },
+]
