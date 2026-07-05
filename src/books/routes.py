ade03a3d5cd5a6c443dbe6156c86f94fd7d4bdf2
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-07-05 13:47:06 +0300

    Finishing access token validation

--- src/books/routes.py
+++ src/books/routes.py
@@ -18,13 +18,13 @@ async def get_all_books(session:AsyncSession = Depends(get_session), user_detail
     return books
 
 @book_router.post("/", status_code=status.HTTP_201_CREATED, response_model=BookModel)
-async def create_book(book_data: BookCreateModel, session:AsyncSession = Depends(get_session)) -> dict:
+async def create_book(book_data: BookCreateModel, session:AsyncSession = Depends(get_session), user_details=Depends(access_token_bearer)) -> dict:
     new_book = await book_service.create_books(book_data, session)
     return new_book
 
 
 @book_router.get("/{book_uid}")
-async def get_book(book_uid: uuid.UUID, session:AsyncSession = Depends(get_session)) -> BookModel:
+async def get_book(book_uid: uuid.UUID, session:AsyncSession = Depends(get_session), user_details=Depends(access_token_bearer)) -> BookModel:
     book = await book_service.get_books(book_uid, session)
     
     if book is not None:
@@ -33,7 +33,7 @@ async def get_book(book_uid: uuid.UUID, session:AsyncSession = Depends(get_sessi
         raise  HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
         
 @book_router.patch("/{book_uid}")
-async def update_book(book_uid: uuid.UUID, book_update_data: BookUpdateModel, session:AsyncSession = Depends(get_session)) -> BookModel:
+async def update_book(book_uid: uuid.UUID, book_update_data: BookUpdateModel, session:AsyncSession = Depends(get_session), user_details=Depends(access_token_bearer)) -> BookModel:
    
    updated_book = await book_service.update_books(book_uid, book_update_data, session)
    
@@ -44,7 +44,7 @@ async def update_book(book_uid: uuid.UUID, book_update_data: BookUpdateModel, se
    
    
 @book_router.delete("/{book_uid}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_uid: uuid.UUID, session:AsyncSession = Depends(get_session)):
+async def delete_book(book_uid: uuid.UUID, session:AsyncSession = Depends(get_session), user_details=Depends(access_token_bearer)):
     book_to_delete = await book_service.delete_books(book_uid, session)
     
     if book_to_delete:
