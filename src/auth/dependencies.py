f0322607053fae9453ff7b2c76748608d4f042f3
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-07-06 16:20:20 +0300

    redis integration, and loggout logic

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -3,6 +3,7 @@ from fastapi.security import HTTPBearer
 from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
 from fastapi.exceptions import HTTPException
+from src.db.redis import token_in_blocklist
 
 class TokenBearer(HTTPBearer):
     
@@ -20,8 +21,20 @@ class TokenBearer(HTTPBearer):
         
         if token_data is None:
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, 
-                detail="Invalid or expired token"
+                status_code=status.HTTP_403_FORBIDDEN, 
+                detail={
+                    'error': 'This token is invalid or expired',
+                    'resolution': 'Please get a new token'
+                }
+            )
+        
+        if await token_in_blocklist(token_data['jti']):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, 
+                detail={
+                    'error': 'This token is invalid or has been revoked',
+                    'resolution': 'Please get a new token'
+                }
             )
             
         self.verify_token_data(token_data)
@@ -31,7 +44,7 @@ class TokenBearer(HTTPBearer):
     def token_valid(self,token:str) -> bool:
         token_data = decode_token(token)
         
-        return True if token_data is not None else False
+        return token_data is not None 
     
     def verify_token_data(self, token_data):
         raise NotImplementedError('Please Override this method in child classes')
--- src/auth/routes.py
+++ src/auth/routes.py
@@ -7,8 +7,8 @@ from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
 from .utils import create_access_token, decode_token, verify_password
 from datetime import timedelta, datetime
-from .dependencies import RefreshTokenBearer
-
+from .dependencies import RefreshTokenBearer, AccessTokenBearer
+from src.db.redis import add_jti_to_blocklist
 
 auth_router = APIRouter()
 user_service = UserService()
@@ -89,5 +89,18 @@ async def refresh_access_token(token_details: dict = Depends(RefreshTokenBearer(
         return JSONResponse(content={
             "access_token": new_access_token
         })
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid or expired refresh     token')
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid or expired refresh token')
+
+@auth_router.get('/logout')
+async def revoke_token(token_details:dict = Depends(AccessTokenBearer())):
     
+    jti = token_details['jti']
+    
+    await add_jti_to_blocklist(jti)
+    
+    return JSONResponse(
+        content={
+            'message': 'Logged out successfully'
+        },
+        status_code=status.HTTP_200_OK
+    )
